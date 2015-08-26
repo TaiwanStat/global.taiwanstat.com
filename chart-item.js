986ecebba68d0c51efbf09d42466dc1304500670
chihsuan <chihsuan.huang.7@gmail.com>
2015-08-27 00:02:34 +0800

    add r to chart-item

--- chart-item.js
+++ chart-item.js
@@ -1,10 +1,12 @@
 var path = require('path');
 
 var long_lists = require('./l/lists.json');
+var real_lists = require('./r/lists.json');
 var partial_arr = ["./include/partials/header.js", "./include/partials/start.js", "./include/partials/end.js", "./include/partials/footer.js"];
 var post_arr = [];
 
 post_arr.push(long_lists);
+post_arr.push(real_lists);
 
 long_lists.data.page.forEach(function(p) {
   post_arr.push({
@@ -16,5 +18,18 @@ long_lists.data.page.forEach(function(p) {
     "filename": path.join('l', p.url, "index.html")
   })
 })
+
+real_lists.data.page.forEach(function(p) {
+  post_arr.push({
+    "data": {
+      "chart_description": p
+    },
+    "partials": partial_arr,
+    "layout": path.join('r', p.url, "index.hbs"),
+    "filename": path.join('r', p.url, "index.html")
+  })
+})
+
+
 
 module.exports = post_arr;
